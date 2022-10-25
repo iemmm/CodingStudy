0492bee7483527695c283660ede8a67209b011e6
jialing <995872429@qq.com>
2022-10-26 00:09:18 +0800

    22-10-26

---
+++ 4字符串/剑指Offer58-II.左旋转字符串/solution.cpp
@@ -0,0 +1,45 @@
+/*
+ *字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * */
+//代码随想录
+class Solution {
+public:
+    void revers(string& s, int left, int right){
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    string reverseLeftWords(string s, int n) {
+        revers(s, 0, n - 1);
+        revers(s, n , s.size() - 1);
+        revers(s, 0, s.size() - 1);
+        return s;
+        }
+// a b c d e f g
+// 0 1 2 3 4 5
+// c b a g f e d
+// d e f g a b c
+};
+
+//自己想得
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        for(int i = 1; i <= n; i++){
+            char temp = s[0];
+            for(int j = 0; j < s.size() - 1; j++){
+                s[j] = s[j + 1];
+            }
+            s[s.size() - 1] = temp;
+        }
+        return s;
+    }
+};
