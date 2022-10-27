275b6a71625b02c0fc6237528dcd664dccee1356
iemmm <chenjialing333@163.com>
2022-10-27 20:20:36 +0800

    22-10-27

--- 4字符串/28找出字符串中第一个匹配项的下标/solution.cpp
+++ 4字符串/28找出字符串中第一个匹配项的下标/solution.cpp
@@ -24,3 +24,39 @@ public:
         return -1;      
     }
 };
+
+class Solution {
+public:
+//KMP算法
+    void getnxet(int* next, string& s){
+        int j = 0;
+        next[0] = 0;
+        for(int i = 1; i < s.size(); i++){
+            while(j > 0 && s[i] != s[j]){
+                j = next[j - 1];
+            }
+            if(s[i] == s[j]){
+                j++;
+            }
+            next[i] = j;
+            }
+        }
+    int strStr(string haystack, string needle) {
+        int next[needle.size()];
+        getnxet(next, needle);
+        int j = 0;
+        for(int i = 0; i < haystack.size(); i++){
+            while(j > 0 && haystack[i] != needle[j]){
+                j = next[j - 1];
+            }
+            if(haystack[i] == needle[j]){
+                j++;
+            }
+            if(j == needle.size()){
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
+
+};
