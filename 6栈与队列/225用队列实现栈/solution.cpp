487808a0e8cebd0170b81c46b901f75211aedf98
iemmm <chenjialing333@163.com>
2022-11-05 21:34:06 +0800

    22-11-5

---
+++ 6栈与队列/225用队列实现栈/solution.cpp
@@ -0,0 +1,62 @@
+/**
+ *请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+
+实现 MyStack 类：
+
+void push(int x) 将元素 x 压入栈顶。
+int pop() 移除并返回栈顶元素。
+int top() 返回栈顶元素。
+boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+ 
+
+注意：
+
+你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/implement-stack-using-queues
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * */
+
+class MyStack {
+public:
+    queue<int> que;
+    MyStack() {
+
+    }
+
+    void push(int x) {
+        que.push(x);
+    }
+
+    int pop() {
+        int size = que.size();
+        size--;
+        while(size--){
+            que.push(que.front());
+            que.pop();
+        }
+        int res = que.front();
+        que.pop();
+        return res;
+    }
+
+    int top() {
+        return que.back();
+    }
+
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
